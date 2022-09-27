15ae0f21471e3a3783c27b0ab51f42660e645f6d
Alex Kiss <alex@sysrex.com>
2022-09-27 21:35:42 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+# ZSH
+if [ "$NO_ZSH" != "true" ]; then
+  # switch to zsh
+  sudo apt install zsh -y
+  sudo chsh gitpod -s "$(which zsh)"
+
+  # Fix GP alias
+  echo "alias pod=/usr/bin/gp" >>~/.zshrc
+
+  # install ohmyzsh
+  CHSH=no
+  export CHSH # fix broken chsh check
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+  # install plugins
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  echo "plugins+=zsh-autosuggestions" >>~/.zshrc
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  echo "plugins+=zsh-syntax-highlighting" >>~/.zshrc
+  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+  echo "fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src" >>~/.zshrc
+
+  echo ZSH_THEME="apple" >>~/.zshrc
+
+  # reload oh-my-zsh instructions again
+  echo "source \$ZSH/oh-my-zsh.sh" >>~/.zshrc
+fi
